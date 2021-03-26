ce7a0baba86aa924a3ed46a50900f250344993c6
aljoschaN91 <70570811+AljoschaNy@users.noreply.github.com>
2021-03-26 10:28:01 +0100

    Add fortune module

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import Fortune from './includes/fortune';
+
 const questionOrigin = "What is your question?";
 const buttonOrigin = "Show Fortune";
 
@@ -51,44 +53,3 @@ button.addEventListener('click', function(){
         fortune.innerHTML = Fortune.getFortune();
     }  
 });
-
-// Fortune object to switch fortunes
-const Fortune = {
-    eightBall: '',
-    switchFortune(){
-        let randomNumber = Math.floor(Math.random()*8);
-    
-        switch(randomNumber) {
-            case 0:
-                eightBall = 'It is certain';
-                break;
-            case 1:
-                eightBall = 'It is decidedly so';
-                break;
-            case 2:
-                eightBall = 'Reply hazy try again';
-                break;
-            case 3:
-                eightBall = 'Cannot predict now';
-                break;
-            case 4:
-                eightBall = 'Do not count on it';
-                break;
-            case 5:
-                eightBall = 'My sources say no';
-                break;
-            case 6:
-                eightBall = 'Outlook not so good';
-                break;
-            case 7:
-                eightBall = 'Signs point to yes';
-                break;
-            default:
-                break;
-        }
-    },
-    getFortune() {
-        this.switchFortune();
-        return eightBall;
-    }
-};
